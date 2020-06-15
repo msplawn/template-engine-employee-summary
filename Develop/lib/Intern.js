4a723b2db04f1ff7795a6e1a9128b85c0e1eb51d
msplawn <mmsplawn@gmail.com>
2020-06-15 18:40:07 -0500

    created classes????

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+class Intern extends Employee{
+    constructor(Intern)   {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.role = Employee
+
+        super(this.school);
+        this.school = school;
+    };
+
+    getGithub() {
+        this.school = school;
+    };
+
+    getRole()   {
+        this.role = "Intern";
+    }
+}
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,18 @@
+const Employee = require("./Employee");
+
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+class Manager extends Employee {
+    constructor(Manager) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.role = Employee
+
+        super(officeNumber);
+        this.officeNumber = officeNumber;
+    }
+
+    getRole()   {
+        this.role = "Manager";
+    }
+}
