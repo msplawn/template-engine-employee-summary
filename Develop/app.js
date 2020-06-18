945b648012a1007a9e1677500637238543f82821
msplawn <mmsplawn@gmail.com>
2020-06-18 18:57:48 -0500

    updated readme and fixed path names for github organization. also fixed question syntax

--- Develop/app.js
+++ Develop/app.js
@@ -83,17 +83,17 @@ function appMenu ()   {
             },
             {
                 type: 'input',
-                message: 'Please enter your employee ID number.',
+                message: 'Please enter engineer employee ID number.',
                 name: 'id'
             },
             {
                 type: 'input',
-                message: 'Please enter your email.',
+                message: 'Please enter engineer email.',
                 name: 'email'
             },
             {
                 type: 'input',
-                message: 'Please enter your GitHub.',
+                message: 'Please enter engineer GitHub.',
                 name: 'github'
             }
         ])
@@ -116,17 +116,17 @@ function appMenu ()   {
             },
             {
                 type: 'input',
-                message: 'Please enter your employee ID number.',
+                message: 'Please enter intern employee ID number.',
                 name: 'id'
             },
             {
                 type: 'input',
-                message: 'Please enter your email.',
+                message: 'Please enter intern email.',
                 name: 'email'
             },
             {
                 type: 'input',
-                message: 'Please enter your school.',
+                message: 'Please enter intern school.',
                 name: 'school'
             }
         ])
@@ -150,82 +150,3 @@ function appMenu ()   {
 appMenu();
 
 
-// const questions = [
-//     {
-//         type: 'input',
-//         message: 'Please enter your full name.',
-//         name: 'fullName'
-//     },
-//     {
-//         type: 'input',
-//         message: 'Please enter your employee ID number.',
-//         name: 'id'
-//     },
-//     {
-//         type: 'input',
-//         message: 'Please enter your email.',
-//         name: 'email'
-//     },
-//     {
-//         type: 'list',
-//         message: 'Please enter your role.',
-//         choices: ['Manager', 'Engineer', 'Intern'],
-//         name: 'role'
-//     },
-// ];
-
-// const inquireQ = () => {
-//     inquirer
-//         .prompt([
-//             {
-//                 type: 'list',
-//                 message: 'What would you like to do?',
-//                 choices: ['Build team', 'Finish team'],
-//                 name: 'moreTeam'
-//             }
-//         ]).then(res => {
-//             const moreTeam = res.moreTeam;
-
-//             switch (moreTeam) {
-//                 case "Build team":
-//                     inquirer.prompt(questions)
-//                         .then(response => {
-//                             if (response.role === "Manager") {
-//                                 inquirer.prompt({
-//                                     type: 'input',
-//                                     message: 'Please enter manager office phone number.',
-//                                     name: 'officeNumber'
-//                                 })
-//                             }
-//                         })
-//             }
-//         })
-
-// }
-
-//-----------------------------------------------------
-
-// inquirer
-//     .prompt(questions);
-
-//-----------------------------------------------------
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
--- README.md
+++ README.md
@@ -3,17 +3,26 @@
 # **Project Description**
 For this project I modified existing code to create a CLI driven application that prompts a manager to create cards for all relevant team members and display them in a relevant way. Managers can create a team via the required classes the build their team and render it to an HTML file. All classes are required to pass tests to validate them.
 
+# **Screencaps:**
+![Code](./assets/invscode.png)
+
+![Code](./assets/inbrowser.png)
+
 # **Demo Video:**
-[https://drive.google.com/file/d/1nJ_z3mpmxPfkcHX7algCj0zJuq4xtFlj/view]
+[https://drive.google.com/file/d/1Rp3A5gqIXETpRQcXIrvwFpsWsMaiew4w/view]
+
+# **Installation**
+Download the zip file and unzip it. Open the files and run npm install to intall the required dependencies. Then run node app and answer the prompted questions. Upon completion check the output folder to view the team.html file in your browser.
+
 
 # **Technology Stack**
 * HTML 
 * CSS
 * Javascript
 * Bootstrap
-<!-- * Node
+* Node
 * Inquirer
-* Jest -->
+* Jest
 
 # **Contact**
 * ### **Name:**  Morgan Splawn @msplawn
